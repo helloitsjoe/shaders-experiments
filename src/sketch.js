243db64ec5ab1e74edfb49af28417fc1f4a0d46f
helloitsjoe <jboyle617@gmail.com>
2020-08-07 15:43:43 -0400

    Swirlies

--- src/sketch.js
+++ src/sketch.js
@@ -1,46 +1,45 @@
 const canvasSketch = require('canvas-sketch');
 
-const GRID_SIZE = 20;
+const GRID_SIZE = 40;
+const MARGIN_X = 70;
+const MARGIN_Y = 70;
 
 const settings = {
   dimensions: [512, 512],
 };
-
-// const makeGrid = (size = 5) => {
-//   const makeRow = () => new Array(size).fill(0);
-//   const grid = new Array(size).fill(makeRow());
-//   return grid;
-// };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const marginX = 70;
-    const marginY = 70;
+    const shrinkPctX = (width - MARGIN_X * 2) / width;
+    const shrinkPctY = (height - MARGIN_Y * 2) / height;
 
-    const shrinkPctX = (width - marginX * 2) / width;
-    const shrinkPctY = (height - marginY * 2) / height;
-
-    const spaceX = (width * shrinkPctX) / (GRID_SIZE - 1);
-    const spaceY = (height * shrinkPctY) / (GRID_SIZE - 1);
+    const spaceX = (1 / (GRID_SIZE - 1)) * shrinkPctX;
+    const spaceY = (1 / (GRID_SIZE - 1)) * shrinkPctY;
 
     for (let y = 0; y < GRID_SIZE; y++) {
       for (let x = 0; x < GRID_SIZE; x++) {
-        const normalX = spaceX * x + marginX;
-        const normalY = spaceY * y + marginY;
+        const normalX = spaceX * x * width + MARGIN_X;
+        const normalY = spaceY * y * width + MARGIN_Y;
 
-        context.lineWidth = 3;
-        context.strokeStyle = 'black';
-        context.beginPath();
-        context.arc(normalX, normalY, 20, 0, 2 * Math.PI);
-        context.stroke();
-        // console.log(normalX, normalY);
+        const size = Math.random() * 10;
+        // const size = 10;
+
+        // context.lineWidth = 3;
+        // context.strokeStyle = 'black';
+        context.font = '18px Arial';
+        context.fillStyle = 'black';
+        context.fillText('=', normalX, normalY);
+        context.translate(normalX, normalY);
+        context.rotate(Math.PI / size);
+        context.translate(-normalX, -normalY);
+        // context.beginPath();
+        // context.arc(normalX, normalY, size, 0, 2 * Math.PI);
+        // context.stroke();
       }
     }
-
-    // console.log(grid);
   };
 };
 
